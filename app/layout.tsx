f5a9b40529e4371892f8edbd149ad540be1633d3
yzhqzrn47-creator <yzhqzrn47@gmail.com>
2026-08-03 02:24:14 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,7 +83,7 @@ export default function RootLayout({
             </a>
             <span>|</span>
             <a 
-              href="https://shop.fitcor.online/pages/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%97%D7%95%D7%91%D7%AA-%D7%94%D7%A4%D7%A8%D7%9F%D7%99%D7%95%D7%AA" 
+              href="https://shop.fitcor.online/policies/privacy-policy" 
               target="_blank" 
               rel="noopener noreferrer" 
               style={{ color: "#A1A1AA", textDecoration: "none" }}
@@ -92,7 +92,7 @@ export default function RootLayout({
             </a>
             <span>|</span>
             <a 
-              href="https://shop.fitcor.online/pages/%D7%94%D7%A6%D7%97%D7%A8%D7%AA-%D7%A0%D7%92%D7%99%D7%A9%D7%95%D7%AA" 
+              href="https://shop.fitcor.online/policies/accessibility-policy" 
               target="_blank" 
               rel="noopener noreferrer" 
               style={{ color: "#A1A1AA", textDecoration: "none" }}
